8b189743b7c1e7c8cfbe6010136719aab909fa0d
unknown <aaradhyvijaywat@gmail.com>
2024-04-29 06:31:24 +0530

    final commit before presentation

---
+++ app/visualize/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+import { Visualize } from "@/components/component/visualize";
+
+export default function Page()
+{
+    return (
+        <>
+        <Visualize />
+        </>
+    )
+}
